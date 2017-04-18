2f3912e3963ac32aa5253792ea0735a6fab867ab
hiro mbp <guest@guest.com>
2017-04-18 15:32:22 +0900

    [update](ver1.0) MainViewController(メイン画面)へIBOutlet追加 + 使用しているコントロールのファイルを追加

--- src/ModCalc/MainViewController.swift
+++ src/ModCalc/MainViewController.swift
@@ -2,9 +2,7 @@
 //  MainViewController.swift
 //  ModCalc
 //
-//  Created by Hiroaki Fujisawa on 2017/04/05.
-//  Copyright © 2017年 xinext. All rights reserved.
-//
+
 
 import UIKit
 
@@ -14,22 +12,57 @@ class MainViewController: UIViewController {
     @IBOutlet weak var outletMainContentsView: UIView!
     @IBOutlet weak var outletMainContentsBottomLayoutConstraint: NSLayoutConstraint!
     
-    // MARK: - Private
+    //タイトルエリア
+    @IBOutlet weak var outletNavigationItem: UINavigationItem!  // アプリタイトル
+    @IBOutlet weak var outletHistoryButton: UIBarButtonItem!    // 履歴ボタン
+    
+    // 表示エリア
+    @IBOutlet weak var outletInputValuesTextField: UITextField!         // 入力値テキスト
+    @IBOutlet weak var outletExpressionLabel: XIPaddingLabel!           // 式タイトルラベル
+    @IBOutlet weak var outletExpressionValueTextField: UITextField!     // 式テキスト
+    @IBOutlet weak var outletAnswerLabel: XIPaddingLabel!               // 答えタイトル
+    @IBOutlet weak var outletAnswerValueTextField: UITextField!         // 答えテキスト
+    @IBOutlet weak var outletDecimalPointTitleLabel: XIPaddingLabel!    // 小数点位置タイトルラベル
+    @IBOutlet weak var outletDecimalPointDownButton: XIPaddingButton!   // 小数点Downボタン
+    @IBOutlet weak var outletDecimalPointValueLabel: XIPaddingLabel!    // 小数点位置テキスト
+    @IBOutlet weak var outletDecimalPointUpButton: XIPaddingButton!     // 小数点Upボタン
+    
+    // 入力エリア
+    @IBOutlet weak var outletKey0Button: XIPaddingButton!               // 0キーボタン
+    @IBOutlet weak var outletKey00Button: XIPaddingButton!              // 00キーボタン
+    @IBOutlet weak var outletKey1Button: XIPaddingButton!               // 1キーボタン
+    @IBOutlet weak var outletKey2Button: XIPaddingButton!               // 2キーボタン
+    @IBOutlet weak var outletKey3Button: XIPaddingButton!               // 3キーボタン
+    @IBOutlet weak var outletKey4Button: XIPaddingButton!               // 4キーボタン
+    @IBOutlet weak var outletKey5Button: XIPaddingButton!               // 5キーボタン
+    @IBOutlet weak var outletKey6Button: XIPaddingButton!               // 6キーボタン
+    @IBOutlet weak var outletKey7Button: XIPaddingButton!               // 7キーボタン
+    @IBOutlet weak var outletKey8Button: XIPaddingButton!               // 8キーボタン
+    @IBOutlet weak var outletKey9Button: XIPaddingButton!               // 9キーボタン
+    @IBOutlet weak var outletKeyDotButton: XIPaddingButton!             // 小数点キーボタン
+    @IBOutlet weak var outletKeyACButton: XIPaddingButton!              // AC(All Clear)キーボタン
+    @IBOutlet weak var outletKeyBSButton: XIPaddingButton!              // BS(Back space)キーボタン
+    @IBOutlet weak var outletKeyDivideButton: XIPaddingButton!          // ÷キーボタン
+    @IBOutlet weak var outletKeyEqualButton: XIPaddingButton!           // =キーボタン
+
+    // MARK: - Private variable
     private var adMgr = AdModMgr()
     
+    // MARK: - ViewController Override
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         // AdMod manager
-        adMgr.initManager(pvc:self, cv:outletMainContentsView, lc: outletMainContentsBottomLayoutConstraint)
+        adMgr.InitManager(pvc:self, cv:outletMainContentsView, lc: outletMainContentsBottomLayoutConstraint)
+        
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        adMgr.dispAdView(pos: AdModMgr.DISP_POSITION.BOTTOM)
-
+        self.initEachView()
+        adMgr.DispAdView(pos: AdModMgr.DISP_POSITION.BOTTOM)
     }
     
     override func didReceiveMemoryWarning() {
@@ -37,7 +70,45 @@ class MainViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK: - Internal method
 
+    // MARK: - private method
+    /**
+     Initialize each view
+     */
+    private func initEachView() {
+        
+        //
+        outletExpressionLabel.FontSizeToFit()
+        outletAnswerLabel.FontSizeToFit()
+        outletDecimalPointTitleLabel.FontSizeToFit()
+        outletDecimalPointDownButton.FontSizeToFit()
+        outletDecimalPointValueLabel.FontSizeToFit()
+        outletDecimalPointUpButton.FontSizeToFit()
+        outletKey0Button.FontSizeToFit()
+        outletKey00Button.FontSizeToFit()
+        
+        outletKey7Button.FontSizeToFit()
+        outletKey8Button.FontSizeToFit()
+        outletKey9Button.FontSizeToFit()
+        outletKeyACButton.FontSizeToFit()
+        outletKey4Button.FontSizeToFit()
+        outletKey5Button.FontSizeToFit()
+        outletKey6Button.FontSizeToFit()
+        outletKeyBSButton.FontSizeToFit()
+        outletKey1Button.FontSizeToFit()
+        outletKey2Button.FontSizeToFit()
+        outletKey3Button.FontSizeToFit()
+        outletKeyDotButton.FontSizeToFit()
+        outletKeyDivideButton.FontSizeToFit()
+        outletKeyEqualButton.FontSizeToFit()
+        
+        outletInputValuesTextField.isUserInteractionEnabled = false
+        outletExpressionValueTextField.isUserInteractionEnabled = false
+        outletAnswerValueTextField.isUserInteractionEnabled = false
+        
+    }
+    
     /*
     // MARK: - Navigation
 
---
+++ src/ModCalc/XIPaddingLabel.swift
@@ -0,0 +1,139 @@
+//
+//  XIPaddingLabel.swift
+//
+
+
+import UIKit
+
+/**
+ - UIButton with character size adjustment function
+ - Author: xinext HF
+ - Copyright: xinext
+ - Date: 2017/04/12
+ - Version: 1.0.0
+ */
+class XIPaddingLabel: UILabel {
+
+    // MARK: - private variable
+    private var _paddingTop: CGFloat = 5
+    private var _paddingBottom : CGFloat = 5
+    private var _paddingLeft: CGFloat = 5
+    private var _paddingRight: CGFloat = 5
+    
+    // MARK: - Interface Bilder property
+    @IBInspectable var PaddingTop: CGFloat {
+        get {
+            return _paddingTop
+        }
+        set {
+            _paddingTop = newValue
+        }
+    }
+    
+    @IBInspectable var PaddingBottom: CGFloat {
+        get {
+            return _paddingBottom
+        }
+        set {
+            _paddingBottom = newValue
+        }
+    }
+    
+    @IBInspectable var PaddingRight: CGFloat {
+        get {
+            return _paddingRight
+        }
+        set {
+            _paddingRight = newValue
+        }
+    }
+    
+    @IBInspectable var PaddingLeft: CGFloat {
+        get {
+            return _paddingLeft
+        }
+        set {
+            _paddingLeft = newValue
+        }
+    }
+    
+    // MARK: - Public method
+    /**
+     Adjust font size according to padding setting.
+     */
+    public func FontSizeToFit() {
+        
+        self.fontSizeToFit(minimumFontScale: 0.2, diminishRate: 0.5)
+    }
+    
+    /**
+     (sub)Adjust font size according to padding setting.
+     - minimumFontScale: CGFloat: minimum font scale
+     - diminishRate: CGFloat: diminish rate of font size
+     */
+    private func fontSizeToFit(minimumFontScale: CGFloat, diminishRate: CGFloat) {
+        
+        let padding = UIEdgeInsets(top: _paddingTop, left: _paddingLeft, bottom: _paddingBottom, right: _paddingRight)
+        
+        let text = self.text
+        if (text!.characters.count == 0) {
+            return
+        }
+        var size = self.frame.size
+        size.height = (size.height) - (padding.bottom + padding.top)
+        size.width = (size.width) - (padding.left + padding.right)
+        
+        let fontName = self.font.fontName
+        let fontSize = self.font.pointSize
+        let minimumFontSize = fontSize * minimumFontScale
+        let isOneLine = (self.numberOfLines == 1)
+        
+        var boundingSize = CGSize.zero
+        var area = CGSize.zero
+        var font = UIFont()
+        var fs = fontSize
+        var newAttributes = [String : Any]()
+        self.attributedText?.enumerateAttributes(in: NSMakeRange(0, text!.characters.count), options: .longestEffectiveRangeNotRequired, using: {
+            (attributes: [String : Any], range: NSRange, stop: UnsafeMutablePointer<ObjCBool>) -> Void in
+            newAttributes = attributes
+        })
+        if newAttributes.count == 0 {
+            return
+        }
+        while (true) {
+            font = UIFont(name: fontName, size: fs)!
+            newAttributes[NSFontAttributeName] = font
+            
+            if isOneLine {
+                boundingSize = CGSize(width: CGFloat(MAXFLOAT), height: size.height)
+            }
+            else {
+                boundingSize = CGSize(width: size.width, height: CGFloat(MAXFLOAT))
+            }
+            area = NSString(string: text!).boundingRect(with: boundingSize, options: .usesLineFragmentOrigin, attributes: newAttributes, context: nil).size
+            
+            if isOneLine {
+                if (area.width <= size.width && area.height <= size.height ) {
+                    break;
+                }
+            }
+            else {
+                if area.height <= size.height {
+                    break;
+                }
+            }
+            
+            if fs == minimumFontSize {
+                break;
+            }
+            
+            fs -= diminishRate
+            if fs < minimumFontSize {
+                fs = minimumFontSize
+            }
+        }
+        
+        self.font = font
+    }
+
+}
