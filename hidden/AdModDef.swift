32f1af82ba2e855145e1e37fbddf2607a87933fe
hiro mbp <guest@guest.com>
2017-04-21 13:25:36 +0900

    [add] 他のパソコン(環境)でもコンパイルできる様に対応

---
+++ hidden/AdModDef.swift
@@ -0,0 +1,10 @@
+//
+//  AdModDef.swift
+//
+//  注意：GitHub等にアップロードしない様にする為、必ず「.gitignore」に含める事
+import Foundation
+
+// *** The unit ID of AdMod for this application.
+let ADMOD_UNITID: String = "ca-app-pub-xxxxxxxxxxxxxxxx/xxxxxxxxxx"
+let ADMOD_TESTID: String = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+
